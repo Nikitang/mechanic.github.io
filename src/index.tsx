99bf2b1d8baf90bad42cca884e334bf26167e2e0
Nick <jaman228822@mail.ru>
2025-03-11 17:57:58 +0500

    hash

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import './styles/index.scss';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
